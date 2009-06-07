5c5d515c380ec3a732a8b56f102d44ec78c34a20
Stephen Kelly <steveire@gmail.com>
2009-06-07 19:59:42 +0100

    Add some more scripts. A Doxyfile, a release file and a valgrind suppression file.

---
+++ scripts/makerelease.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+if [ -n "$1" ]
+then
+  version=$1
+else
+  echo "This script takes one argument- a version string."
+  exit
+fi
+
+tempDir="/tmp/grantlee-release"
+
+if [ ! -d "$tempDir" ]
+then
+  mkdir $tempDir
+fi
+
+echo Creating $tempDir/grantlee-$version.tar.gz
+
+git archive --format=tar --prefix=grantlee-$version/ HEAD | gzip > $tempDir/grantlee-$version.tar.gz
+
+echo Unpacking to $tempDir/grantlee-$version
+tar -C $tempDir -xvf $tempDir/grantlee-$version.tar.gz
+
+if [ "$?" -ne "0" ]
+then
+  echo "Error creating tar archive."
+  exit
+fi
+
+oldDir=$PWD
+
+echo Creating build directory
+mkdir -p $tempDir/grantlee-$version/build && cd $tempDir/grantlee-$version/build
+
+echo Building.
+cmake -DCMAKE_INSTALL_PREFIX=../prefix ..
+make && make install
+
+echo "Testing" 
+cd tests && ctest
+
+
+echo Copying archive to $oldDir/grantlee-$version.tar.gz
+cp $tempDir/grantlee-$version.tar.gz $oldDir
+
