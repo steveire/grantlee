6edd282664861a5849ddb56909f51c594b149a5e
Stephen Kelly <steveire@gmail.com>
2010-01-30 18:06:43 +0100

    Simplify the GRANTLEE_EXPORT macro now that I know why it exists.
    Also use the correct defined symbol name as set in the target properties.
    Now it can actually work.

--- grantlee_core_library/grantlee_export.h
+++ grantlee_core_library/grantlee_export.h
@@ -20,39 +20,13 @@
 #ifndef GRANTLEE_EXPORT_H
 #define GRANTLEE_EXPORT_H
 
-// TODO: Figure out the Qt way to do libraries. This is copied from kdemacros.h
-#define __HAVE_GCC_VISIBILITY
-
-#ifdef __HAVE_GCC_VISIBILITY
-#define KDE_NO_EXPORT __attribute__ ((visibility("hidden")))
-#define KDE_EXPORT __attribute__ ((visibility("default")))
-#define KDE_IMPORT __attribute__ ((visibility("default")))
-#elif defined(_WIN32) || defined(_WIN64)
-#define KDE_NO_EXPORT
-#define KDE_EXPORT __declspec(dllexport)
-#define KDE_IMPORT __declspec(dllimport)
-#else
-#define KDE_NO_EXPORT
-#define KDE_EXPORT
-#define KDE_IMPORT
-#endif
-
-
 #ifndef GRANTLEE_EXPORT
-# if defined(MAKE_GRANTLEE_LIB)
-/* We are building this library */
-#  define GRANTLEE_EXPORT KDE_EXPORT
-// #  define GRANTLEE_EXPORT Q_DECL_EXPORT
-# else
-/* We are using this library */
-#  define GRANTLEE_EXPORT KDE_IMPORT
-// #  define GRANTLEE_EXPORT Q_DECL_IMPORT
-# endif
+#  if defined(GRANTLEE_CORE_LIB_MAKEDLL)
+#    define GRANTLEE_EXPORT __declspec(dllexport)
+#  else
+#    define GRANTLEE_EXPORT __declspec(dllimport)
+#  endif
 #endif
-
-# ifndef GRANTLEE_EXPORT_DEPRECATED
-#  define GRANTLEE_EXPORT_DEPRECATED KDE_DEPRECATED GRANTLEE_EXPORT
-# endif
 
 #endif
 
