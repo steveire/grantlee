a4719ae1724852ee1da6e06f1b8409094fd348c8
Stephen Kelly <steveire@gmail.com>
2010-01-30 09:25:07 +0100

    Add missing licence header.

--- grantlee_core_library/enginestate_p.h
+++ grantlee_core_library/enginestate_p.h
@@ -1,3 +1,21 @@
+/*
+  This file is part of the Grantlee template system.
+
+  Copyright (c) 2009 Stephen Kelly <steveire@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 3 only, as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License version 3 for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #ifndef ENGINE_STATE_P_H
 #define ENGINE_STATE_P_H
@@ -5,7 +23,8 @@
 #include "enginestate.h"
 #include "templateloader.h"
 
-namespace Grantlee {
+namespace Grantlee
+{
 
 class EngineStateImplPrivate
 {
