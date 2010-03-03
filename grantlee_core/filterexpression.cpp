ada65fe2e0a961ee394c8f5c994b7ccab1727063
Stephen Kelly <steveire@gmail.com>
2010-03-03 16:40:32 +0100

    Change the licence from LGPLv3 to LGPLv2+
    This makes it legal to use it in GPLv2 libraries.

--- grantlee_core/filterexpression.cpp
+++ grantlee_core/filterexpression.cpp
@@ -1,16 +1,17 @@
 /*
   This file is part of the Grantlee template system.
 
-  Copyright (c) 2009 Stephen Kelly <steveire@gmail.com>
+  Copyright (c) 2009,2010 Stephen Kelly <steveire@gmail.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
-  License version 3 only, as published by the Free Software Foundation.
+  License as published by the Free Software Foundation; either version
+  2 of the Licence, or (at your option) any later version.
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License version 3 for more details.
+  Library General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
@@ -243,3 +244,4 @@ QStringList FilterExpression::filters() const
   Q_D( const FilterExpression );
   return d->m_filterNames;
 }
+
