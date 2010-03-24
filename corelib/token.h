16451476609ebe67ba289fac4eb73752c9cb6698
Stephen Kelly <steveire@gmail.com>
2010-03-24 01:45:36 +0100

    Finish partially completed example in documentation.

--- corelib/token.h
+++ corelib/token.h
@@ -47,7 +47,7 @@ enum TokenType {
 */
 struct Token {
   int tokenType;    ///< The Type of this Token
-  int linenumber;
+  int linenumber;   ///< The line number this Token starts at
   QString content;  ///< The content of this Token
 };
 
