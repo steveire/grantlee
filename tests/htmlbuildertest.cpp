5b7da2ab7eb61fa15e0c68eb7bd6e6d5fc68c8b6
Stephen Kelly <steveire@gmail.com>
2010-10-06 20:43:10 +0200

    Update the license to be consistent with what is in the COPYING file.

--- tests/htmlbuildertest.cpp
+++ tests/htmlbuildertest.cpp
@@ -4,8 +4,8 @@
     Copyright (c) 2008 Stephen Kelly <steveire@gmail.com>
 
     This library is free software; you can redistribute it and/or modify it
-    under the terms of the GNU Library General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or (at your
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or (at your
     option) any later version.
 
     This library is distributed in the hope that it will be useful, but WITHOUT
@@ -13,7 +13,7 @@
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
     License for more details.
 
-    You should have received a copy of the GNU Library General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this library; see the file COPYING.LIB.  If not, write to the
     Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
     02110-1301, USA.
