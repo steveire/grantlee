steveire/grantlee

commit 0ee5d83db7e172a9da77f42c07cecdb61916be0c

Ensure that we never decrement a uint below 0.

templates/lib/engine.cpp
@@ -177,7 +177,10 @@ void Engine::loadDefaultLibraries()
         break;
       }
 
-      PluginPointer<TagLibraryInterface> library = d->loadCppLibrary( libName, minorVersion-- );
+      PluginPointer<TagLibraryInterface> library = d->loadCppLibrary( libName, minorVersion );
+      if ( minorVersion == 0)
+        break;
+      minorVersion--;
       if ( library )
         break;
     }
@@ -197,7 +200,10 @@ TagLibraryInterface* Engine::loadLibrary( const QString &name )
 
   uint minorVersion = GRANTLEE_VERSION_MINOR;
   while ( minorVersion >= GRANTLEE_MIN_PLUGIN_VERSION ) {
-    TagLibraryInterface* library = d->loadLibrary( name, minorVersion-- );
+    TagLibraryInterface* library = d->loadLibrary( name, minorVersion );
+    if (minorVersion == 0)
+      break;
+    minorVersion--;
     if ( library )
       return library;
   }

commit ef7c99c031d3e486c55afb1fb4d328b505456af3

Return the library if it is found.
This fixes a regression in commit 0ee5d83d (Ensure that we never
decrement a uint below 0., 2013-09-02).

templates/lib/engine.cpp
@@ -201,11 +201,11 @@ TagLibraryInterface* Engine::loadLibrary( const QString &name )
   uint minorVersion = GRANTLEE_VERSION_MINOR;
   while ( minorVersion >= GRANTLEE_MIN_PLUGIN_VERSION ) {
     TagLibraryInterface* library = d->loadLibrary( name, minorVersion );
+    if ( library )
+      return library;
     if (minorVersion == 0)
       break;
     minorVersion--;
-    if ( library )
-      return library;
   }
   throw Grantlee::Exception( TagSyntaxError, QString::fromLatin1( "Plugin library '%1' not found." ).arg( name ) );
   return 0;