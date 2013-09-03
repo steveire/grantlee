bb919acc78824179c15a9aaa220dfb43f9a5ac10
Stephen Kelly <steveire@gmail.com>
2013-09-03 10:33:42 +0200

    Remove grantlee_add_plugin macro. Add macro to adjust plugin name.
    The grantlee_add_plugin macro predates automoc, and was partly
    added to simplify moc'ing. Now we expect downstreams to use
    CMAKE_AUTOMOC or to otherwise handle the moc issue themselves.
    
    Additionally, make downstream linking to grantlee_core more explicit
    by not hiding it in a macro.
    
    The code to add all sources to a global property is specific to
    the Grantlee build for coverage analysis, and is not relevant to
    downstreams anyway.
    
    Add a porting guide to document such changes affecting users.

--- cmake/modules/GrantleeUse.cmake
+++ cmake/modules/GrantleeUse.cmake
@@ -7,36 +7,10 @@ set(CMAKE_MODULE_PATH ${Grantlee_MODULE_DIR} ${CMAKE_MODULE_PATH})
 
 include(CMakeParseArguments)
 
-macro(GRANTLEE_ADD_PLUGIN pluginname)
-  set(options)
-  set(oneValueArgs)
-  set(multiValueArgs TAGS FILTERS)
-
-  cmake_parse_arguments(_PLUGIN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-  foreach(_filename ${_PLUGIN_UNPARSED_ARGUMENTS})
-    get_source_file_property(_skip ${_filename}.h SKIP_AUTOMOC)
-    if (NOT _skip)
-      list(APPEND _headers ${_filename}.h)
-    endif()
-    list(APPEND _sources ${_filename}.cpp)
-  endforeach()
-  foreach(_filename ${_PLUGIN_TAGS})
-    list(APPEND _headers ${_filename}.h)
-    list(APPEND _sources ${_filename}.cpp)
-  endforeach()
-  foreach(_filename ${_PLUGIN_FILTERS})
-    list(APPEND _sources ${_filename}.cpp)
-  endforeach()
-
-  add_library(${pluginname} MODULE ${_sources} ${_plugin_moc_srcs})
-
+macro(grantlee_adjust_plugin_name pluginname)
   set_target_properties(${pluginname}
     PROPERTIES PREFIX ""
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee_VERSION_MAJOR}.${Grantlee_VERSION_MINOR}"
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee_VERSION_MAJOR}.${Grantlee_VERSION_MINOR}"
-  )
-  target_link_libraries(${pluginname}
-    grantlee_core
   )
 endmacro()
