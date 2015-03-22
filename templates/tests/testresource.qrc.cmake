7de49877441ae7ae28429743cef3918b62cdbefb
Stephen Kelly <steveire@gmail.com>
2015-03-22 23:57:07 +0100

    Make it possible to build without Qt5LinguistTools.
    Conditionally build the relevant test.

--- templates/tests/testresource.qrc.cmake
+++ templates/tests/testresource.qrc.cmake
@@ -3,8 +3,5 @@
     <file alias="scripteddefaults.qs">@PROJECT_SOURCE_DIR@/scripteddefaults.qs</file>
     <file alias="testtags.qs">@PROJECT_SOURCE_DIR@/testtags.qs</file>
 </qresource>
-<qresource>
-    <file alias="test_de_DE.qm">@PROJECT_BINARY_DIR@/test_de_DE.qm</file>
-    <file alias="test_fr_FR.qm">@PROJECT_BINARY_DIR@/test_fr_FR.qm</file>
-</qresource>
+@QM_FILES@
 </RCC>
