5d6e38b01780f23fbe1951cfe3a4d6a1eff259b0
Stephen Kelly <steveire@gmail.com>
2015-03-22 16:03:05 +0100

    Sync the version variable names used in the buildsystem.
    The internally used variable names were not the same as the ones
    generated in the Config.cmake files for example.

--- cmake/modules/GrantleeMacros.cmake
+++ cmake/modules/GrantleeMacros.cmake
@@ -4,7 +4,7 @@ include(CMakeParseArguments)
 macro(grantlee_adjust_plugin_name pluginname)
   set_target_properties(${pluginname}
     PROPERTIES PREFIX ""
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee_VERSION_MAJOR}.${Grantlee_VERSION_MINOR}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee_VERSION_MAJOR}.${Grantlee_VERSION_MINOR}"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
   )
 endmacro()
--- templates/tests/testresource.qrc.cmake
+++ templates/tests/testresource.qrc.cmake
@@ -1,5 +1,5 @@
 <!DOCTYPE RCC><RCC version="1.0">
-<qresource prefix="/plugins/grantlee/@Grantlee_MAJOR_MINOR_VERSION_STRING@">
+<qresource prefix="/plugins/grantlee/@Grantlee5_MAJOR_MINOR_VERSION_STRING@">
     <file alias="scripteddefaults.qs">@PROJECT_SOURCE_DIR@/scripteddefaults.qs</file>
     <file alias="testtags.qs">@PROJECT_SOURCE_DIR@/testtags.qs</file>
 </qresource>
