8a1e2c72a4e98143afd8f862fbfa89b415dfffde
Stephen Kelly <steveire@gmail.com>
2019-11-26 22:51:51 +0000

    Remove use of PROJECT_{SOURCE,BINARY}_DIR
    This only works because of the excess project() commands in the
    buildsystem. The CMAKE_CURRENT_{SOURCE,BINARY}_DIR are more resilient to
    being moved around.

--- templates/tests/testresource.qrc.cmake
+++ templates/tests/testresource.qrc.cmake
@@ -1,12 +1,12 @@
 <!DOCTYPE RCC><RCC version="1.0">
 <qresource prefix="/plugins/grantlee/@Grantlee5_MAJOR_MINOR_VERSION_STRING@">
-    <file alias="scripteddefaults.qs">@PROJECT_SOURCE_DIR@/scripteddefaults.qs</file>
-    <file alias="testtags.qs">@PROJECT_SOURCE_DIR@/testtags.qs</file>
+    <file alias="scripteddefaults.qs">@CMAKE_CURRENT_SOURCE_DIR@/scripteddefaults.qs</file>
+    <file alias="testtags.qs">@CMAKE_CURRENT_SOURCE_DIR@/testtags.qs</file>
 </qresource>
 
 <qresource prefix="/templates/">
-    <file alias="resourcetemplate1.html">@PROJECT_SOURCE_DIR@/resourcetest/resourcetemplate1.html</file>
-    <file alias="resourcetemplate2.html">@PROJECT_SOURCE_DIR@/resourcetest/resourcetemplate2.html</file>
+    <file alias="resourcetemplate1.html">@CMAKE_CURRENT_SOURCE_DIR@/resourcetest/resourcetemplate1.html</file>
+    <file alias="resourcetemplate2.html">@CMAKE_CURRENT_SOURCE_DIR@/resourcetest/resourcetemplate2.html</file>
 </qresource>
 
 @QM_FILES@
