9ead7ba71690e778d2f71d6bd00d016a530ea622
Stephen Kelly <steveire@gmail.com>
2019-12-04 21:34:25 +0000

    Use primitives instead of relying on custom variable

--- cmake/modules/GrantleeMacros.cmake
+++ cmake/modules/GrantleeMacros.cmake
@@ -4,16 +4,16 @@ include(CMakeParseArguments)
 macro(grantlee_adjust_plugin_name pluginname)
   set_target_properties(${pluginname}
     PROPERTIES PREFIX ""
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_MAJOR_MINOR_VERSION_STRING}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_MAJOR_MINOR_VERSION_STRING}"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
     DEBUG_POSTFIX "d"
   )
   foreach(cfg ${CMAKE_CONFIGURATION_TYPES})
     string(TOUPPER ${cfg} CFG)
     set_target_properties(${pluginname}
       PROPERTIES
-      LIBRARY_OUTPUT_DIRECTORY_${CFG} "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_MAJOR_MINOR_VERSION_STRING}"
-      RUNTIME_OUTPUT_DIRECTORY_${CFG} "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_MAJOR_MINOR_VERSION_STRING}"
+      LIBRARY_OUTPUT_DIRECTORY_${CFG} "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+      RUNTIME_OUTPUT_DIRECTORY_${CFG} "${CMAKE_BINARY_DIR}/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
       )
   endforeach()
 endmacro()
