cutelyst/cutelee

filesizeformat implementation as well as localized filesize (#2)
* filesizeformat implementation as well as localized filesize

This implements the filesizeformat filter as it is available
in Django (plus some optional features). Additionally it
introduces two new tags (l10n_filesize and l10n_filesize_var)
for the i18n system to support file sizes with localized number
strings.

Originally from: https://github.com/steveire/grantlee/pull/36

* Use QLocale::formattedDataSize() if available (Qt 5.10+)

If build against Qt 5.10 or newer, internally QLocale::formattedDataSize()
will be used for values that fit into a qint64. The old implementation
is still there for older Qt versions and for values that do not fit into
a qint64.

The main difference bitween the internal implementation in utils.cpp as
calcFileSize() and QLocale::formattedDataSize() is that QLocale also
translates the unit name.

templates/i18n/l10n_filesize.h
@@ -1,0 +1,90 @@
+/*
+ * This file is part of the Cutelee template system.
+ *
+ * Copyright (c) 2020 Matthias Fehring <mf@huessenbergnetz.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef L10N_FILESIZE_H
+#define L10N_FILESIZE_H
+
+#include "node.h"
+
+namespace Cutelee
+{
+    class Parser;
+}
+
+using namespace Cutelee;
+
+class L10nFileSizeNodeFactory : public AbstractNodeFactory
+{
+    Q_OBJECT
+public:
+    L10nFileSizeNodeFactory();
+
+    Node *getNode(const QString &tagContent, Parser *p) const override;
+};
+
+class L10nFileSizeVarNodeFactory : public AbstractNodeFactory
+{
+    Q_OBJECT
+public:
+    L10nFileSizeVarNodeFactory();
+
+    Node *getNode(const QString &tagContent, Parser *p) const override;
+};
+
+class L10nFileSizeNode : public Node
+{
+    Q_OBJECT
+public:
+    L10nFileSizeNode(const FilterExpression &size,
+                     const FilterExpression &unitSystem,
+                     const FilterExpression &precision,
+                     const FilterExpression &multiplier,
+                     QObject *parent = nullptr);
+
+    void render(OutputStream *stream, Context *c) const override;
+
+private:
+    FilterExpression m_size;
+    FilterExpression m_unitSystem;
+    FilterExpression m_precision;
+    FilterExpression m_multiplier;
+};
+
+class L10nFileSizeVarNode : public Node
+{
+    Q_OBJECT
+public:
+    L10nFileSizeVarNode(const FilterExpression &size,
+                        const FilterExpression &unitSystem,
+                        const FilterExpression &precision,
+                        const FilterExpression &multiplier,
+                        const QString &resultName,
+                        QObject *parent = nullptr);
+
+    void render(OutputStream *stream, Context *c) const override;
+
+private:
+    FilterExpression m_size;
+    FilterExpression m_unitSystem;
+    FilterExpression m_precision;
+    FilterExpression m_multiplier;
+    QString m_resultName;
+};
+
+#endif

steveire/grantlee

templates/i18n/l10n_filesize.h
@@ -1,0 +1,89 @@
+/*
+ * This file is part of the Grantlee template system.
+ *
+ * Copyright (c) 2020 Matthias Fehring <mf@huessenbergnetz.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef L10N_FILESIZE_H
+#define L10N_FILESIZE_H
+
+#include "node.h"
+
+namespace Grantlee
+{
+class Parser;
+}
+
+using namespace Grantlee;
+
+class L10nFileSizeNodeFactory : public AbstractNodeFactory
+{
+  Q_OBJECT
+public:
+  L10nFileSizeNodeFactory();
+
+  Node *getNode(const QString &tagContent, Parser *p) const override;
+};
+
+class L10nFileSizeVarNodeFactory : public AbstractNodeFactory
+{
+  Q_OBJECT
+public:
+  L10nFileSizeVarNodeFactory();
+
+  Node *getNode(const QString &tagContent, Parser *p) const override;
+};
+
+class L10nFileSizeNode : public Node
+{
+  Q_OBJECT
+public:
+  L10nFileSizeNode(const FilterExpression &size,
+                   const FilterExpression &unitSystem,
+                   const FilterExpression &precision,
+                   const FilterExpression &multiplier,
+                   QObject *parent = nullptr);
+
+  void render(OutputStream *stream, Context *c) const override;
+
+private:
+  FilterExpression m_size;
+  FilterExpression m_unitSystem;
+  FilterExpression m_precision;
+  FilterExpression m_multiplier;
+};
+
+class L10nFileSizeVarNode : public Node
+{
+  Q_OBJECT
+public:
+  L10nFileSizeVarNode(const FilterExpression &size,
+                      const FilterExpression &unitSystem,
+                      const FilterExpression &precision,
+                      const FilterExpression &multiplier,
+                      const QString &resultName, QObject *parent = nullptr);
+
+  void render(OutputStream *stream, Context *c) const override;
+
+private:
+  FilterExpression m_size;
+  FilterExpression m_unitSystem;
+  FilterExpression m_precision;
+  FilterExpression m_multiplier;
+  QString m_resultName;
+};
+
+#endif